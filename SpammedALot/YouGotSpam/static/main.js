ac03d4708f02da0c0273313361c8d7511e5b5c82
Angelo Williams <gelo@Angelos-MBP-2.hitronhub.home>
2023-03-15 20:36:36 -0700

    javascript and python are finally communicating

--- SpammedALot/YouGotSpam/static/main.js
+++ SpammedALot/YouGotSpam/static/main.js
@@ -1,1 +1,41 @@
-console.log('Javascript is working!');
+let userInfo = {
+    email: '',
+    password: ''
+};
+let go = document.querySelector('#spam-button');
+let results = document.querySelector('#lower-wrapper');
+
+
+go.addEventListener('click', () => {
+    getUserData();
+});
+
+function getUserData() {
+    userInfo.email = document.querySelector('input#email_').value;
+    userInfo.password = document.querySelector('input#password_').value;
+    document.querySelector('input#email_').value = "";
+    document.querySelector('input#password_').value = "";
+
+
+    console.log('email: ', userInfo.email);
+    console.log('password: ', userInfo.password);
+
+    results.classList.remove('hide_');
+    sendRequest(userInfo);
+}
+
+function sendRequest(data) {
+    // send a POST request to the user-data endpoint
+    let xhr = new XMLHttpRequest();
+    xhr.open('POST', '/user-data/', true);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.setRequestHeader("X-CSRFToken", document.getElementsByName("csrfmiddlewaretoken")[0].value);
+    console.log("data: ", JSON.stringify(data))
+    xhr.onreadystatechange = () => {
+        if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+            // handle response from Django Views modules
+            console.log('xhr.responseText: ', xhr.responseText);
+        }
+    };
+    xhr.send(JSON.stringify(data))
+}
--- SpammedALot/YouGotSpam/views.py
+++ SpammedALot/YouGotSpam/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+import json
 
 # Create your views here.
 
+# Render the most descendant extension template to render it and all
 def home(request):
-    return render(request, 'base.html', {})
+    return render(request, 'results.html', {})
+
+
+# Endpoint for getting user input
+def user_data(request):
+    if request.method == "POST":
+        data = json.loads(request.body)
+        email = data.get("email")
+        password = data.get("password")
+
+        return JsonResponse({'message': 'User data submitted.'})
+    else:
+        return JsonResponse({'error': 'Invalid request'})
